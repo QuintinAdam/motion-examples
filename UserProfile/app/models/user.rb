ace9ad704406410fd861d2e39ec29cf73e2d52fc
Quintin Adam <quintinadam@live.com>
2015-06-03 10:54:28 -0600

    encode and decode

--- UserProfile/app/models/user.rb
+++ UserProfile/app/models/user.rb
@@ -12,4 +12,23 @@ class User
       end
     end
   end
+
+  # make User NSCoding compliant
+  # decode and encode our data
+  # deserialize our object out of the decoder instance
+  def initWithCoder(decoder)
+    self.init
+    PROPERTIES.each do |prop|
+      saved_value = decoder.decodeObjectForKey(prop.to_s)
+      self.send("#{prop}=", saved_value)
+    end
+    self
+  end
+
+  # gets called when we want to save our object and encode its properties with encodeObject:forKey
+  def encodeWithCoder(encoder)
+    PROPERTIES.each do |prop|
+      encoder.encodeObject(self.send(prop), forKey: prop.to_s)
+    end
+  end
 end
