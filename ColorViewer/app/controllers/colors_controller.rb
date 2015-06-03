4e6e93b1a2faac7491f07b1b3f8af5a17dedb8a9
Quintin Adam <quintinadam@live.com>
2015-06-03 00:15:46 -0600

    basic color controller

---
+++ ColorViewer/app/controllers/colors_controller.rb
@@ -0,0 +1,16 @@
+class ColorsController < UIViewController
+  # Subclassing UIViewController always involves overriding viewDidLoad()
+  # This method is called right after our controller's view
+  # viewDidLoad() is one of the view life-cycle methods.
+  # Every controller's view goes through several stages: creation, appearance, disappearance, and destruction.
+  def viewDidLoad
+    super
+    self.view.backgroundColor = UIColor.whiteColor
+    @label = UILabel.alloc.initWithFrame(CGRectZero)
+    @label.text = "Colors"
+    @label.sizeToFit
+    @label.center = [self.view.frame.size.width / 2, self.view.frame.size.height / 2]
+    @label.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin
+    self.view.addSubview(@label)
+  end
+end
