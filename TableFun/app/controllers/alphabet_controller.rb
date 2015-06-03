aaac27665d500a22d498e14e125614b4f380858e
Quintin Adam <quintinadam@live.com>
2015-06-03 15:44:15 -0600

    table cells

--- TableFun/app/controllers/alphabet_controller.rb
+++ TableFun/app/controllers/alphabet_controller.rb
@@ -6,6 +6,34 @@ class AlphabetController < UIViewController
     @table = UITableView.alloc.initWithFrame(self.view.bounds)
     @table.autoresizingMask = UIViewAutoresizingFlexibleHeight
     self.view.addSubview(@table)
+
+    # A table's dataSource must implement the following methods, or else the app will throw an exception: tableView:cellForRowAtIndexPath: and tableView:numberOfRowsInSection:
+    @table.dataSource = self
+
+    # numberOfRowsInSection: should return exactly what it sounds like. cellForRowAtIndex- Path:'s job is to either create a new cell or recycle an offscreen one.
+    # cellForRowAtIndexPath: uses two unfamiliar objects: indexPath is an instance of NSIndexPath, and the method is supposed to return a UITableViewCel
+
+    # def tableView(tableView, numberOfRowsInSection: section)
+    # # return the number of rows
+    # end
+    # def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    #   # return the UITableViewCell for the row
+    # end
+
+    @data = ("A".."Z").to_a
+
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    # return the UITableViewCell for the row
+    @data.count
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    @reuseIdentifier ||= "CELL_IDENTIFIER"
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier)
+    cell ||= UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier) cell.textLabel.text = @data[indexPath.row]
+    cell
   end
 
 end
