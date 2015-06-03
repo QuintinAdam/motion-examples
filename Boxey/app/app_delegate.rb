2bd4bc1d85b60245913f1270749174d7435ffdbe
Quintin Adam <quintinadam@live.com>
2015-06-02 20:22:24 -0600

    look ma! a blue box

--- Boxey/app/app_delegate.rb
+++ Boxey/app/app_delegate.rb
@@ -4,6 +4,9 @@ class AppDelegate
     @window.backgroundColor = UIColor.whiteColor
     @window.makeKeyAndVisible # This is a special method for UIWindow that tells the system it will be the window receiving touch events and should be drawn to the screen.
 
+    @blue_view = UIView.alloc.initWithFrame(CGRect.new([10, 40], [100, 100]))
+    @blue_view.backgroundColor = UIColor.blueColor
+    @window.addSubview(@blue_view)
     true
   end
 end
