30787616e68a770c2aa43107aa04899af41a6b4e
Quintin Adam <quintinadam@live.com>
2015-06-02 22:14:19 -0600

    tapping box

--- Boxey/app/app_delegate.rb
+++ Boxey/app/app_delegate.rb
@@ -11,12 +11,23 @@ class AppDelegate
     @add_button = UIButton.buttonWithType(UIButtonTypeSystem)
     @add_button.setTitle("Add", forState:UIControlStateNormal)
     @add_button.sizeToFit
-    @add_button.frame = CGRect.new(
-      [10, @window.frame.size.height - 10 - @add_button.frame.size.height],
-      @add_button.frame.size
-    )
+    @add_button.frame = CGRect.new([10, @window.frame.size.height - 10 - @add_button.frame.size.height], @add_button.frame.size)
     @window.addSubview(@add_button)
+
+    @add_button.addTarget(self, action: "add_tapped", forControlEvents:UIControlEventTouchUpInside)
 
     true
   end
+
+  def add_tapped
+  new_view = UIView.alloc.initWithFrame(CGRect.new([0, 0], [100, 100]))
+  new_view.backgroundColor = UIColor.blueColor
+  last_view = @window.subviews[0]
+  new_view.frame = CGRect.new(
+      [last_view.frame.origin.x,
+        last_view.frame.origin.y + last_view.frame.size.height + 10],
+      last_view.frame.size)
+    @window.insertSubview(new_view, atIndex:0)
+  end
+
 end
