a5ddf64a1f5f21a610f72138dd9ba804398fc4e9
Quintin Adam <quintinadam@live.com>
2015-06-04 09:55:40 -0600

    adds did select row

--- TableFun/app/controllers/alphabet_controller.rb
+++ TableFun/app/controllers/alphabet_controller.rb
@@ -9,6 +9,7 @@ class AlphabetController < UIViewController
 
     # A table's dataSource must implement the following methods, or else the app will throw an exception: tableView:cellForRowAtIndexPath: and tableView:numberOfRowsInSection:
     @table.dataSource = self
+    @table.delegate = self
 
     # numberOfRowsInSection: should return exactly what it sounds like. cellForRowAtIndex- Path:'s job is to either create a new cell or recycle an offscreen one.
     # cellForRowAtIndexPath: uses two unfamiliar objects: indexPath is an instance of NSIndexPath, and the method is supposed to return a UITableViewCel
@@ -34,6 +35,21 @@ class AlphabetController < UIViewController
     cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier)
     cell ||= UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier) cell.textLabel.text = @data[indexPath.row]
     cell
+  end
+
+  def tableView(tableView, didSelectRowAtIndexPath:indexPath)
+    tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    letter = @data[indexPath.row]
+
+    controller = UIViewController.alloc.initWithNibName(nil, bundle:nil)
+    controller.view.backgroundColor = UIColor.whiteColor
+    controller.title = letter
+    label = UILabel.alloc.initWithFrame(CGRectZero)
+    label.text = letter
+    label.sizeToFit
+    label.center = [controller.view.frame.size.width / 2, controller.view.frame.size.height / 2]
+    controller.view.addSubview(label)
+    self.navigationController.pushViewController(controller, animated:true)
   end
 
 end
