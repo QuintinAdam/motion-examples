bb0dde53b119db7c375bf3c53545f439bd1be405
Quintin Adam <quintinadam@live.com>
2015-06-04 12:22:13 -0600

    fixes tablefun

--- TableFun/app/app_delegate.rb
+++ TableFun/app/app_delegate.rb
@@ -2,8 +2,7 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @alphabet_controller = AlphabetController.alloc.initWithNibName(nil, bundle:nil)
-    @window.rootViewController =
-      UINavigationController.alloc.initWithRootViewController(@alphabet_controller)
+    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(@alphabet_controller)
     @window.makeKeyAndVisible
 
     true
