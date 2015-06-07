adececdc739bcfef056ddda379a7ba2e3aa4128d
Quintin Adam <quintinadam@live.com>
2015-06-07 09:56:23 -0600

    fixes rake file

---
+++ Colr/spec/controller_spec.rb
@@ -0,0 +1,12 @@
+describe "The color controller" do
+  tests ColorController
+
+  it "loads the controller fine" do
+    controller.should.not == nil
+  end
+
+  # it "has a twitter label" do
+  #   controller.instance_variable_get("@twitter").should.not == nil
+  # end
+
+end
