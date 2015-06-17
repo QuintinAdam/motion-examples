18a3f8e9184b576fdf19c998b69d47c61b5991ab
Quintin Adam <quintinadam@live.com>
2015-06-16 22:34:40 -0600

    adds style to fine label search box and button

--- rmq-giphy-motion/app/stylesheets/main_stylesheet.rb
+++ rmq-giphy-motion/app/stylesheets/main_stylesheet.rb
@@ -6,7 +6,7 @@ class MainStylesheet < ApplicationStylesheet
   end
 
   def root_view(st)
-    st.background_color = color.white
+    st.background_color = color.light_gray
   end
 
   def hello_world(st)
@@ -16,5 +16,27 @@ class MainStylesheet < ApplicationStylesheet
     st.font = font.medium
     st.text = 'Hello World'
   end
+
+  def search_label(st)
+    st.frame = {l: 10, t: 100, w: rmq.device.width- 20, h: 40}
+    st.text = 'Find'
+    st.color = color.white
+    st.font = font.large
+  end
+
+  def query(st)
+    st.frame = {l: 10, t: 150, w: rmq.device.width - 20, h: 40}
+    st.background_color = color.white
+    st.view.font = font.large
+  end
+
+  def submit_button(st)
+    st.frame = {l: 10, t: 200, w: rmq.device.width - 20, h: 40}
+    st.background_color = color.blue
+    st.text = 'Go!'
+    st.color = color.white
+    st.font = font.large
+  end
+
 
 end
