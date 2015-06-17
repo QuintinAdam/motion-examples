5615a466e97bc7905b408731d04f7395110937f5
Quintin Adam <quintinadam@live.com>
2015-06-16 22:09:38 -0600

    starts adding controller

--- rmq-giphy-motion/app/controllers/main_controller.rb
+++ rmq-giphy-motion/app/controllers/main_controller.rb
@@ -11,46 +11,54 @@ class MainController < UIViewController
     rmq(self.view).apply_style :root_view
 
     # Create your UIViews here
-    @hello_world_label = rmq.append!(UILabel, :hello_world)
-  end
+    # @hello_world_label = rmq.append!(UILabel, :hello_world)
 
-  def init_nav
-    self.title = 'Title Here'
+    rmq.append(UILabel, :search_label)
 
-    self.navigationItem.tap do |nav|
-      nav.leftBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemAction,
-                                                                           target: self, action: :nav_left_button)
-      nav.rightBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemRefresh,
-                                                                           target: self, action: :nav_right_button)
+    @query = rmq.append(UITextField, :query).get
+
+    rmq.append(UIButton, :submit_button).on(:touch) do |sender|
+      puts "button tapped"
     end
   end
 
-  def nav_left_button
-    puts 'Left button'
+  def init_nav
+    self.title = 'Giphy'
+
+    # self.navigationItem.tap do |nav|
+    #   nav.leftBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemAction,
+    #                                                                        target: self, action: :nav_left_button)
+    #   nav.rightBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemRefresh,
+    #                                                                        target: self, action: :nav_right_button)
+    # end
   end
 
-  def nav_right_button
-    puts 'Right button'
-  end
+  # def nav_left_button
+  #   puts 'Left button'
+  # end
+
+  # def nav_right_button
+  #   puts 'Right button'
+  # end
 
   # Remove these if you are only supporting portrait
-  def supportedInterfaceOrientations
-    UIInterfaceOrientationMaskAll
-  end
-  def willAnimateRotationToInterfaceOrientation(orientation, duration: duration)
-    # Called before rotation
-    rmq.all.reapply_styles
-  end
-  def viewWillLayoutSubviews
-    # Called anytime the frame changes, including rotation, and when the in-call status bar shows or hides
-    #
-    # If you need to reapply styles during rotation, do it here instead
-    # of willAnimateRotationToInterfaceOrientation, however make sure your styles only apply the layout when
-    # called multiple times
-  end
-  def didRotateFromInterfaceOrientation(from_interface_orientation)
-    # Called after rotation
-  end
+  # def supportedInterfaceOrientations
+  #   UIInterfaceOrientationMaskAll
+  # end
+  # def willAnimateRotationToInterfaceOrientation(orientation, duration: duration)
+  #   # Called before rotation
+  #   rmq.all.reapply_styles
+  # end
+  # def viewWillLayoutSubviews
+  #   # Called anytime the frame changes, including rotation, and when the in-call status bar shows or hides
+  #   #
+  #   # If you need to reapply styles during rotation, do it here instead
+  #   # of willAnimateRotationToInterfaceOrientation, however make sure your styles only apply the layout when
+  #   # called multiple times
+  # end
+  # def didRotateFromInterfaceOrientation(from_interface_orientation)
+  #   # Called after rotation
+  # end
 end
 
 
