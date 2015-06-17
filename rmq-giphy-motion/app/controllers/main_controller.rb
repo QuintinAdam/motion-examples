3ac4b7de7c736c2068de3aba49d72fbf341abfe7
Quintin Adam <quintinadam@live.com>
2015-06-16 22:44:19 -0600

    search image method

--- rmq-giphy-motion/app/controllers/main_controller.rb
+++ rmq-giphy-motion/app/controllers/main_controller.rb
@@ -3,78 +3,47 @@ class MainController < UIViewController
   def viewDidLoad
     super
 
-    # Sets a top of 0 to be below the navigation control, it's best not to do this
-    # self.edgesForExtendedLayout = UIRectEdgeNone
-
     rmq.stylesheet = MainStylesheet
     init_nav
     rmq(self.view).apply_style :root_view
 
-    # Create your UIViews here
-    # @hello_world_label = rmq.append!(UILabel, :hello_world)
-
     rmq.append(UILabel, :search_label)
 
-    @query = rmq.append(UITextField, :query).get
+    @query = rmq.append(UITextField, :query).focus.get
 
     rmq.append(UIButton, :submit_button).on(:touch) do |sender|
-      puts "button tapped"
+      search_for_images @query.text
     end
   end
 
   def init_nav
     self.title = 'Giphy'
-
-    # self.navigationItem.tap do |nav|
-    #   nav.leftBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemAction,
-    #                                                                        target: self, action: :nav_left_button)
-    #   nav.rightBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemRefresh,
-    #                                                                        target: self, action: :nav_right_button)
-    # end
   end
 
-  # def nav_left_button
-  #   puts 'Left button'
-  # end
+  def search_for_images(query)
 
-  # def nav_right_button
-  #   puts 'Right button'
-  # end
+    if query && (query != '')
+      query = query.gsub(/\s/, '%20')
+      url = "http://api.giphy.com/v1/gifs/search?q=#{query}&api_key=dc6zaTOxFJmzC"
 
-  # Remove these if you are only supporting portrait
-  # def supportedInterfaceOrientations
-  #   UIInterfaceOrientationMaskAll
-  # end
-  # def willAnimateRotationToInterfaceOrientation(orientation, duration: duration)
-  #   # Called before rotation
-  #   rmq.all.reapply_styles
-  # end
-  # def viewWillLayoutSubviews
-  #   # Called anytime the frame changes, including rotation, and when the in-call status bar shows or hides
-  #   #
-  #   # If you need to reapply styles during rotation, do it here instead
-  #   # of willAnimateRotationToInterfaceOrientation, however make sure your styles only apply the layout when
-  #   # called multiple times
-  # end
-  # def didRotateFromInterfaceOrientation(from_interface_orientation)
-  #   # Called after rotation
-  # end
+      rmq.animations.start_spinner
+
+      AFMotion::HTTP.get(url) do |result|
+        if html = result.body
+          images = html.scan(/src=\"(.+?\.jpg)\"/).map do |m|
+            m.first
+          end
+          open_images_controller images if images.length > 0
+        end
+        rmq.animations.stop_spinner
+      end
+    end
+  end
+
+  def open_images_controller(images)
+    controller = ImagesController.new
+    controller.images = images
+    controller.query = @query.text
+    self.navigationController.pushViewController(controller, animated: true)
+  end
 end
-
-
-__END__
-
-# You don't have to reapply styles to all UIViews, if you want to optimize,
-# another way to do it is tag the views you need to restyle in your stylesheet,
-# then only reapply the tagged views, like so:
-def logo(st)
-  st.frame = {t: 10, w: 200, h: 96}
-  st.centered = :horizontal
-  st.image = image.resource('logo')
-  st.tag(:reapply_style)
-end
-
-# Then in willAnimateRotationToInterfaceOrientation
-rmq(:reapply_style).reapply_styles
-
-
